77ae0318f879edbda8c454a9e14a7b5bdcc664ad
Srijana rai <srijanarye330@gmail.com>
2026-02-08 18:37:45 +0545

    add registration,login

--- src/pages/authentication/Login.jsx
+++ src/pages/authentication/Login.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import Form from './components/Form'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const Login = () => {
+  const navigate = useNavigate()
+  const handleLogin = async (data) => {
+    try {
+    const response = await axios.post("https://react30.onrender.com/api/user/login", data)
+      if (response.status === 200) {
+        navigate('/')
+      } else {
+        alert("Login Failed")
+      }
+    } catch (error) {
+      alert(error?.response?.data?.message)
+    }
+  }
   return (
-    <Form type='Login'/>
+    <Form type='Login' onSubmit={handleLogin} />
   )
 }
 
